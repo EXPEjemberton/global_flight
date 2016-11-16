daaf246d009c9eff44a24b3438c765009bf646a7
phuriku <jaemberton@gmail.com>
2016-11-16 15:24:17 -0600

    Add Neo4j city register.

--- src/main/scala/com/flight/services/FlightJoiner.scala
+++
@@ -1,8 +0,0 @@
-package com.flight.services
-
-import com.flight.model.Flight
-
-object FlightJoiner {
-  def cheapestReturnFlights(destFlights: List[Flight], returnFlights: List[Flight]) =
-    
-}
