42795c48e32e9158b24ce81ea20f9b328bfc8d1a
phuriku <jaemberton@gmail.com>
2016-11-16 12:31:10 -0600

    Add FlightJoiner.

---
+++ src/main/scala/com/flight/services/FlightJoiner.scala
@@ -0,0 +1,8 @@
+package com.flight.services
+
+import com.flight.model.Flight
+
+object FlightJoiner {
+  def cheapestReturnFlights(destFlights: List[Flight], returnFlights: List[Flight]) =
+    
+}
